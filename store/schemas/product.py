5566f23ba16160d91b42074e22ae91bda83ea7d5
Ester Adaianne <100163831+eadaianne@users.noreply.github.com>
2025-08-31 11:18:05 -0300

    Update product.py

--- store/schemas/product.py
+++ store/schemas/product.py
@@ -2,8 +2,13 @@ from decimal import Decimal
 from typing import Annotated, Optional
 from bson import Decimal128
 from pydantic import AfterValidator, Field
+from datetime import datetime
 from store.schemas.base import BaseSchemaMixin, OutSchema
 
+def convert_decimal_128(v):
+    return Decimal128(str(v))
+
+Decimal_ = Annotated[Decimal, AfterValidator(convert_decimal_128)]
 
 class ProductBase(BaseSchemaMixin):
     name: str = Field(..., description="Product name")
@@ -11,27 +16,17 @@ class ProductBase(BaseSchemaMixin):
     price: Decimal = Field(..., description="Product price")
     status: bool = Field(..., description="Product status")
 
-
-class ProductIn(ProductBase, BaseSchemaMixin):
+class ProductIn(ProductBase):
     ...
-
 
 class ProductOut(ProductIn, OutSchema):
     ...
-
-
-def convert_decimal_128(v):
-    return Decimal128(str(v))
-
-
-Decimal_ = Annotated[Decimal, AfterValidator(convert_decimal_128)]
-
 
 class ProductUpdate(BaseSchemaMixin):
     quantity: Optional[int] = Field(None, description="Product quantity")
     price: Optional[Decimal_] = Field(None, description="Product price")
     status: Optional[bool] = Field(None, description="Product status")
-
+    updated_at: Optional[datetime] = Field(None, description="Data de atualização do produto")
 
 class ProductUpdateOut(ProductOut):
     ...
